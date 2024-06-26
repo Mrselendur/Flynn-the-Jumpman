9d827fbe8675ecf6e422839af3a70e9e7eba4cb3
Mrselendur <iliyan.markov27@gmail.com>
2024-06-26 19:35:43 +0300

    fixed rockhead blinking

--- Obstacles/rock_head.gd
+++ Obstacles/rock_head.gd
@@ -1,14 +1,14 @@
 extends RigidBody2D
 
-@onready var sprite = $AnimatedSprite2D
+@onready var animated_sprite = $AnimatedSprite2D
 @onready var hitbox = $Hitbox/CollisionShape2D
 @onready var find_ground = $FindGround
 @onready var find_player: Array = $FindPlayer.get_children() #get all raycasts
 
 var init_pos: Vector2                           #Vector2 variable for the starting position 
 
-const BLINK_TIME = 1000                         #blink timer for blink animation (is subject to change!)
-var counter = BLINK_TIME                        #blink counter (is subject to change!)
+const BLINK_TIME := 10.0                        #blink timer for blink animation
+var blinking: bool = false                      #is true when rockhead is blinking
 
 const TIMER := 2.0
 
@@ -16,12 +16,13 @@ func _ready():
 	init_pos = self.global_position        #set initial posotion to the global position in the scene
 
 func _process(_delta):
-	counter -= 1                                #decrease every frame (not good!)
-	if(0 == counter):                           #different systems will get different blink times in seconds!
-		sprite.play("blink")
-		await sprite.animation_finished
-		sprite.play("idle")
-		counter = BLINK_TIME
+	if(false == blinking):
+		blinking = true                                      #cannot blink again before animation is over
+		await get_tree().create_timer(BLINK_TIME).timeout    #blink every 10 seconds
+		animated_sprite.play("blink")
+		await animated_sprite.animation_finished
+		animated_sprite.play("idle")
+		blinking = false            #reset blinking so rockhead can blink again in another 10 seconds
 
 func _physics_process(_delta):
 	for ray in find_player:                               #go through all player raycasts
@@ -33,10 +34,10 @@ func _physics_process(_delta):
 #function to handle when body is on ground
 func on_ground():
 	find_ground.set_deferred("enabled", false)            #disable ground raycast to prevent 
-	sprite.play("hit ground")                             #starting the function more than once
-	await sprite.animation_finished                       #wait for end of animation
+	animated_sprite.play("hit ground")                             #starting the function more than once
+	await animated_sprite.animation_finished                       #wait for end of animation
 	hitbox.set_deferred("disabled", true)                 #disable hitbox while on ground
-	sprite.play("idle")                                   #go back to idle animation
+	animated_sprite.play("idle")                                   #go back to idle animation
 	for ray in find_player:                               #disable all player raycasts
 		ray.set_deferred("enabled", false)
 	await get_tree().create_timer(TIMER).timeout          #wait 2 seconds
