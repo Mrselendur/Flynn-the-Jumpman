1314586752733486f753ff9df78c10d5d72c61f3
Mrselendur <iliyan.markov27@gmail.com>
2024-07-18 02:27:44 +0300

    minor goal tweak

--- Goal/goal.gd
+++ Goal/goal.gd
@@ -8,8 +8,9 @@ func _ready() -> void:
 	self.visible = false
 
 #check every frame for a shown goal and if player has collected required points
-func _process(_delta):
-	if((false == is_shown) && (required_points <= GameManager.getLevelPoints())):
-		self.set_deferred("monitorable", true)          #turn on monitorable property
-		self.set_deferred("visible", true)              #turn on visible property
-		is_shown = true            #set is_shown to true to never enter the if statement agian
+func _process(_delta) -> void:
+	if  is_shown || required_points > GameManager.getLevelPoints():
+		return
+	self.monitorable = true         #make the goal monitorable
+	self.visible = true             #make the goal visible
+	is_shown = true                 #remove the need for the code to execute again
