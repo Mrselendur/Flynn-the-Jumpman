f7496f94b8768512e01af6463d5c03c5311e170c
Mrselendur <iliyan.markov27@gmail.com>
2024-09-03 01:40:21 +0300

    bug fixes

--- Scenes/Levels/Play Level Music.gd
+++ Scenes/Levels/Play Level Music.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 @onready var musicStream: AudioStream = load("res://Free/Audio/Music/" + self.name + ".mp3")
-var scenePath: String = "res://Scenes/Levels/" + self.name + ".tscn"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var scenePath: String = "res://Scenes/Levels/" + self.name + ".tscn"
 	if GameManager.get_current_level() == scenePath:
 		return
 	GameManager.set_current_level(scenePath)
-	AudioHandler.stop_music()
 	AudioHandler.play_music(musicStream)
